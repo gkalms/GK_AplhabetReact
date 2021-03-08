323100b56a6cdf8760384e8ac12bc885ac404417
gkalms <georgina.kalms@gmail.com>
2021-03-08 12:30:11 +1100

    monday 8th Trting to get input to work

--- client/src/Test/List.test.js
+++ client/src/Test/List.test.js
@@ -7,14 +7,43 @@ export const ListTest = (props) => {
     return (
         <div>
             <form>
-                <div >
+                <div className="row" >
                     <Button variant="info" value="A" onClick={props.handleClick}>A</Button>
                     <Button variant="info" value="B" onClick={props.handleClick}>B</Button>
                     <Button variant="info" value="C" onClick={props.handleClick}>C</Button>
-                    <Button variant="info" value="D" onClick={props.handleClick}>E</Button>
-                    <Button variant="info" value="E" onClick={props.handleClick}>A</Button>
+                    <Button variant="info" value="D" onClick={props.handleClick}>D</Button>
+                    <Button variant="info" value="E" onClick={props.handleClick}>E</Button>
+                    <Button variant="info" value="F" onClick={props.handleClick}>F</Button>
+                    <Button variant="info" value="G" onClick={props.handleClick}>G</Button>
                 </div>
-                <P></P>
+                <div className="row">
+                    <Button variant="success" onClick={props.handleClick} value="H">H</Button>
+                    <Button variant="success" onClick={props.handleClick} value="I">I</Button>
+                    <Button variant="success" onClick={props.handleClick} value="J">J</Button>
+                    <Button variant="success" onClick={props.handleClick} value="K">K</Button>
+                    <Button variant="success" onClick={props.handleClick} value="L">L</Button>
+                    <Button variant="success" onClick={props.handleClick} value="M">M</Button>
+                    <Button variant="success" onClick={props.handleClick} value="N">N</Button>
+                </div>
+                <div className="row">
+                    <Button variant="warning" onClick={props.handleClick} value="O">O</Button>
+                    <Button variant="warning" onClick={props.handleClick} value="P">P</Button>
+                    <Button variant="warning" onClick={props.handleClick} value="Q">Q</Button>
+                    <Button variant="warning" onClick={props.handleClick} value="R">R</Button>
+                    <Button variant="warning" onClick={props.handleClick} value="S">S</Button>
+                    <Button variant="warning" onClick={props.handleClick} value="T">T</Button>
+                    <Button variant="warning" onClick={props.handleClick} value="U">U</Button>
+                </div>
+                <div className="row">
+                    <Button variant="info" onClick={props.handleClick} value="V">V</Button>
+                    <Button variant="info" onClick={props.handleClick} value="W">W</Button>
+                    <Button variant="info" onClick={props.handleClick} value="X">X</Button>
+                    <Button variant="info" onClick={props.handleClick} value="Y">Y</Button>
+                    <Button variant="info" onClick={props.handleClick} value="Z">Z</Button>
+                    <Button variant="secondary" id="clear">Clear</Button>
+                    <Button variant="secondary" id="quit">Quit</Button>
+                </div>
+                <p>Input will go here</p>
             </form>
         </div>
     );
--- client/src/Test/container.test.js
+++ client/src/Test/container.test.js
@@ -1,27 +1,25 @@
-// import React, { useEffect, useState } from "react";
 import { ListTest } from "./List.test";
 
 export const Container = () => {
-    //1: get the value when Button clicked
-
-    const handleClickLetter = (e) => {
-        const input = e.currentTarget.value
-        console.log("letter:", input);
-    // 2: Use alphabet value to filter from fetched words and display
-    // API call
     
     const getWordsByLetter = () => {
-            fetch('http://localhost:9000/api/alphabet/words/{input}', {
+            fetch('http://localhost:9000/api/alphabet/words/input', {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
                 },
                 data: JSON.stringify(),
             }).then((response) => {
-                    console.log("response:", response)
+                    console.log("Log 3 Response:", response)
                     return response.json()
                 });
         }
+
+        const handleClickLetter = (e) => {
+            const input = e.currentTarget.value
+            console.log("log 1 Letter:", input);
+            getWordsByLetter(input); 
+            console.log("Log 2 function:", getWordsByLetter);
     }
        
     return (
